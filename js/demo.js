b5dc1dbb9d4c1e96c99e70f00b6e7d3a92db6170
Vasanthan S <vvasanthan636@gmail.com>
2025-12-03 18:43:38 +0530

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,12 @@
+let name = "John";
+let val1=23;
+let val2=34.5;
+let isdatavalil=true;
+let nodata=null;
+let a;
+console.log(name);
+console.log(val1);
+console.log(val2);
+console.log(isdatavalil);
+console.log(nodata);
+console.log(a);
---
+++ js/var.js
@@ -0,0 +1,5 @@
+var a=5;
+if(a<10)
+    var a=50;
+
+console.log(a);     
